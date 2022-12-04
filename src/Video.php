14bb190f37a0d2dc90c213ca4dbb5dd782d56474
André Amorim <andrefcamorim@gmail.com>
2022-12-04 15:28:57 +0000

    Set thumbnail if has it on video

--- src/Video.php
+++ src/Video.php
@@ -27,8 +27,22 @@ class Video {
 						'primary'   => $this->get_group_field( 'appearance_group', 'primary_color', $post_ID ),
 						'secondary' => $this->get_group_field( 'appearance_group', 'second_color', $post_ID ),
 					],
+					'box'      => [
+						'width'  => $this->get_group_field( 'box_group', 'width', $post_ID ),
+						'height' => $this->get_group_field( 'box_group', 'height', $post_ID ),
+
+					],
 					'controls' => $this->get_group_field( 'controls_group', 'controls', $post_ID ),
 				];
+
+				$thumbnail_id = get_post_thumbnail_id( $post_ID );
+				if ( ! empty( $thumbnail_id ) ) {
+					$video_data['thumbnail'] = [
+						'url'    => get_the_post_thumbnail_url( $post_ID ),
+						'srcset' => wp_get_attachment_image_srcset( $thumbnail_id, 'medium' ),
+						'alt'    => get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true ),
+					];
+				}
 
 				return $this->render_video( $video_data );
 
@@ -50,22 +64,34 @@ class Video {
 		//https://codepen.io/VincentGarreau/pen/zLONLe
 		//https://codepen.io/mattimatti/pen/KMPKxK
 
+		$thumbnail = '';
+		if ( isset( $data['thumbnail'] ) ) {
+			//With thumbnail
+			$thumbnail = sprintf(
+				', "poster": "%1$s"',
+				$data['thumbnail']['url'],
+			);
+		}
+
 		return sprintf(
 			'<video
 				id="%1$s"
 				class="video-js vjs-default-skin vjs-big-play-centered vjs-show-big-play-button-on-pause"
-				width="640" height="264"
+				width="%2$s" height="%3$s"
 				preload="auto"
 				playsinline
-				data-setup=\'{ "techOrder": ["%2$s"], "sources": [{ "type": "%3$s", "src": "%4$s"}]}\'
+				data-setup=\'{ "techOrder": ["%4$s"], "sources": [{ "type": "%5$s", "src": "%6$s"}]%7$s}\'
 				controls
-		  	>
-		  	</video>
-		  	',
+			  >
+			  </video>
+			  ',
 			$data['id'],
+			$data['box']['width'],
+			$data['box']['height'],
 			'youtube',
 			'video/youtube',
-			$data['link']
+			$data['link'],
+			$thumbnail,
 		);
 	}
 
